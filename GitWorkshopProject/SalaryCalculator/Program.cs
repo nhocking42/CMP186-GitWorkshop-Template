e5a16bfd5907bf6ecd0a696c3b0737a1acbe54ba
nhocking42 <72036160+nhocking42@users.noreply.github.com>
2022-02-28 08:13:00 +0700

    Update Program.cs

--- GitWorkshopProject/SalaryCalculator/Program.cs
+++ GitWorkshopProject/SalaryCalculator/Program.cs
@@ -8,6 +8,9 @@ namespace SalaryCalculator
         {
             var managerSalary = EmployeeService.CalculateSalary(EmployeeType.Manager, 7);
             Console.WriteLine(string.Format("Manager's Salary: {0}", managerSalary));
+            
+            var ExpertSalary = EmployeeService.CalculateSalary(EmployeeType.Expert, 9);
+            Console.WriteLine(string.Format("Expert's Salary: {0}", ExpertSalary));
 
             Console.ReadLine();
         }
